e2fc34aa20276e3b5575a8b489810c4083e583d0
Monaembs <59017040+Monaembs@users.noreply.github.com>
2019-12-18 13:09:53 +0100

    Update main.c
    first

--- main.c
+++ main.c
@@ -1,9 +1,11 @@
-int main()
-{
-  int i;
-  for (i = 0; i < 10; i++)
-  {
-    printf("Holberton\n");
-  }
-  return 0;
-}
+1    1  int main()
+   2    2  {
+   3    3    int i;
+   4      -  for (i = 0; i < 10; i++)
+        4 +  for (i = 0; i < 32; i++)
+   5    5    {
+   6      -    printf("Holberton\n");
+        6 +    printf("Hello Holberton\n");
+   7    7    }
+   8    8    return 0;
+   9    9  }
